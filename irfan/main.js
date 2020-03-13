e561f8c012ca4e1575f7e2fe952ab60396cb61f6
MuhdIrfan2612 <59907038+MuhdIrfan2612@users.noreply.github.com>
2020-03-13 12:43:59 +0800

    Add files via upload

---
+++ irfan/main.js
@@ -0,0 +1,12 @@
+function menuToggle(){
+    var nav = document.getElementById("nav");
+    nav.classList.toggle('active')
+    var toggle = document.getElementById("toggle");
+    toggle.classList.toggle('active')
+}
+
+// hisham
+// const menuicon = document.querySelector(".menuicon");
+// menuicon.addEventListener("click",function(){
+//     document.querySelector('.nav').classList.toggle("show")
+// })
